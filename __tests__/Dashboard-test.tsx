47f7fda6e89bdac3df4ce6f75177f6cce7e6b4c5
Rushit Jivani <rushitjivani1999@gmail.com>
2023-04-19 17:18:32 +0530

    Update Dashboard-test.tsx

--- __tests__/Dashboard-test.tsx
+++ __tests__/Dashboard-test.tsx
@@ -4,8 +4,8 @@ import ApiHelper from '../src/service/ApiHelper';
 // API test
 describe('Test API', () => {
   const TEST_CASE = [1, 2, 3, 4];
-  TEST_CASE.forEach((item, index) => {
-    test(`Test case ${index}: ${item}`, () => {
+  TEST_CASE.forEach(item => {
+    test(`Test case page: ${item}`, () => {
       return ApiHelper.getStoriesOfSection(item).then(res => {
         expect(res).toBeTruthy();
       });
